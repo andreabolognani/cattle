ae05e4903b7018d7163476a3fa815e173c30daea
Andrea Bolognani <eof@kiyuko.org>
2009-02-03 20:08:00 +0100

    Improve autogen.sh to abort on error with an explanatory message.

--- autogen.sh
+++ autogen.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
 
-#gtkdocize --flavour no-tmpl --copy && \
-libtoolize --force --copy && \
-aclocal && \
-autoheader && \
-automake --copy --add-missing && \
+#gtkdocize --flavour no-tmpl --copy
+#if [ "$?" -ne 0 ]; then echo "gtkdocize failed" >&2; exit 1; fi
+
+libtoolize --force --copy
+if [ "$?" -ne 0 ]; then echo "libtoolize failed" >&2; exit 1; fi
+
+aclocal
+if [ "$?" -ne 0 ]; then echo "aclocal failed" >&2; exit 1; fi
+
+autoheader
+if [ "$?" -ne 0 ]; then echo "autoheader failed" >&2; exit 1; fi
+
+automake --copy --add-missing
+if [ "$?" -ne 0 ]; then echo "automake failed" >&2; exit 1; fi
+
 autoconf
+if [ "$?" -ne 0 ]; then echo "autoconf failed" >&2; exit 1; fi
