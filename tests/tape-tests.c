aade932784bb8015c3b1fbbd31cbd68e2d0945af
Andrea Bolognani <eof@kiyuko.org>
2010-01-02 17:57:06 +0100

    Show support to the no-www campaign.

--- tests/tape-tests.c
+++ tests/tape-tests.c
@@ -16,7 +16,7 @@
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  *
- * Homepage: http://www.kiyuko.org/software/cattle
+ * Homepage: http://kiyuko.org/software/cattle
  */
 
 #include <glib.h>
