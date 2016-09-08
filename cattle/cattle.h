b12f17c0f37e088c53a9ffe11ecfb46ae72df9d0
Andrea Bolognani <eof@kiyuko.org>
2016-09-08 23:06:36 +0200

    Include cattle-enums.h from cattle.h

--- cattle/cattle.h
+++ cattle/cattle.h
@@ -32,6 +32,7 @@
 #include <cattle/cattle-program.h>
 #include <cattle/cattle-configuration.h>
 #include <cattle/cattle-interpreter.h>
+#include <cattle/cattle-enums.h>
 
 #undef __CATTLE_H_INSIDE__
 
