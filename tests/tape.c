a8e8a11bc9870a8539c181d82cda30edee117a33
Andrea Bolognani <eof@kiyuko.org>
2019-04-27 17:43:05 +0200

    Update copyright years
    Signed-off-by: Andrea Bolognani <eof@kiyuko.org>

--- tests/tape.c
+++ tests/tape.c
@@ -1,5 +1,5 @@
 /* tape - Check the tape reports the correct size limits
- * Copyright (C) 2008-2017  Andrea Bolognani <eof@kiyuko.org>
+ * Copyright (C) 2008-2020  Andrea Bolognani <eof@kiyuko.org>
  * This file is part of Cattle
  *
  * This program is free software; you can redistribute it and/or modify
